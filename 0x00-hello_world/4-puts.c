6c212e1a638e3eefccf887835ebd15b6dbef6207
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-03-16 14:23:39 -0700

    Hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *return - Return 0 means program sucess
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
