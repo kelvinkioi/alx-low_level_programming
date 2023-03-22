72855ba6ba4e9ee5402f2451d990309c8a5e7ee6
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-03-22 10:57:59 -0700

    returns the result of two added numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *add - add two integers and returns results
+ *@x: integer
+ *@y: integer
+ *
+ *Return: sum of x and y
+ */
+int add(int x, int y)
+{
+	int z;
+
+	z = x + y;
+	return (z);
+}
+
