55e23f8566447ea18f192b73d958459dfaec637a
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-04-20 06:57:30 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "3-calc.h"
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Void
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+	int (*op_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	op_func = get_op_func(argv[2]);
+
+	if (!op_func)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", op_func(num1, num2));
+
+	return (0);
+}
