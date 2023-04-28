3bce8d965a97d0aa63a0c0be1d91d9b438e74cdb
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-04-28 11:51:20 -0700

    function that executes before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * printer - A function that is executed before the main function
+ * 
+ * Return: Void
+ */
+
+void __attribute__ ((constructor)) printer()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
