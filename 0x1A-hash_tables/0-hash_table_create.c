aec41523f755837e9371fe0e4577f0996d4879dc
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-07-07 12:06:22 -0700

    changes to 1-djb2 file

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,13 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	table = malloc(sizeof(hash_table_t));
 	if (!table)
-		return (0);
+		return (NULL);
 	table->size = size;
 	table->array = calloc(table->size, sizeof(hash_node_t *));
 	if (!(table->array))
 	{
 		free(table);
-		return (0);
+		return (NULL);
 	}
 	return (table);
 }
